fc8e429200f6986ea788f762073f579a027162b4
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-01-20 11:29:27 +0100

    Added phinx as new dependency to make db migrations easily
    Phinx is a PHP database migration tool (see: https://phinx.org/). It allows us to write a migrate an existing database scheme to a new code base.
    
    To create a new migration run 'vendor/bin/phinx create <MigrationName>' this will create a new php-class in the directory db/migrations (this directory should be created first)
    
    To migrate an existing database run the shell script './runMigration.sh'. This makes sure that the db configuration defined in the .env-file will be used.
    
    The file phinx.yml provides the configuration for phinx. In the current configuration it expects that some environment variables are set. The script 'runMigration.sh' ensures this expectation by loading the .env-file and exporting the variables.

---
+++ runMigration.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+envFile=.env
+
+# check preconditions
+if [ ! -f .env ]; then
+    echo "Your environment is not set up correctly! Move the .env.example to .env, and edit your database (DB_*)" \
+            "credentials."
+fi
+
+
+# set up environment variables from .env-file
+while read line   # iterate over lines
+do
+    if [ ! -z ${line}  ]; then
+        declare "${line}"
+    fi
+done <<< "$(cat ${envFile})" # this makes sure that the loop will not be executed in a subshell
+
+export PHINX_DBHOST=${DB_HOST}
+export PHINX_DBNAME=${DB_NAME}
+export PHINX_DBUSER=${DB_USER}
+export PHINX_DBPASS=${DB_PASSWORD}
+export PHINX_DBPORT=${DB_PORT}
+export PHINX_CHARSET=${DB_CHARSET}
+
+vendor/bin/phinx migrate -e production
